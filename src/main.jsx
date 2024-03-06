209253eff8905e37c9b0fb570fed989957549f04
Volodya66 <130245015+Volodya66@users.noreply.github.com>
2024-03-06 20:21:12 +0200

    c

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/ViteReact">
+    <BrowserRouter basename="/Cars-rent">
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,5 @@ export default defineConfig({
           assets: '/src/assets'
         },
       },
-      base: '/ViteReact',
+      base: '/Cars-rent',
     });
